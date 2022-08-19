4dc2a6969c6398bea93b33063f052a935cb2c750
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-19 14:30:44 +0700

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,26 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Row and Column"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Text 1"),
-            Text("Text 2"),
-            Text("Text 3"),
-            Row(
-              children: [
-                Text("Text 4"),
-                Text("Text 5"),
-                Text("Text 6"),
-              ],
-            )
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text("Container Widget"),
+          ),
+          body: Container(
+            color: Colors.amber,
+            // margin: const EdgeInsets.all(10),
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            // padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Container(
+              // color: Colors.indigo,
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: const LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: <Color>[
+                        Colors.amber,
+                        Colors.blue,
+                        Colors.indigo,
+                        Colors.deepPurple
+                      ])),
+            ),
+          )),
     );
   }
 }

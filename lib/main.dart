9a6fa5ae356ac599c6c492fceb3fec1a2fc47e07
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-21 05:52:21 +0700

    Spacer Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Image Widget"),
+          title: const Text("Spacer Widget"),
         ),
         body: Center(
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            child: const Image(
-              // image: NetworkImage("https://images.unsplash.com/photo-1657299156653-d3c0147ba3ee"),
-              image: AssetImage('images/unsplash.jpg'),
-              fit: BoxFit.cover,
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Spacer(
+              flex: 1,
             ),
-          ),
-        ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.red,
+            ),
+            Spacer(
+              flex: 2,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.green,
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            Container(
+              width: 80,
+              height: 80,
+              color: Colors.blue,
+            ),
+            Spacer(),
+          ],
+        )),
       ),
     );
   }

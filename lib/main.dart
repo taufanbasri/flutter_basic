07117f4ab91ecdb0399426d836618c708fc118a3
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-21 03:49:13 +0700

    Stack Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,158 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Animated Container"),
-          ),
-          body: Center(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {});
-              },
-              child: AnimatedContainer(
-                color: Color.fromARGB(255, random.nextInt(256),
-                    random.nextInt(256), random.nextInt(256)),
-                duration: const Duration(seconds: 1),
-                width: 50.0 + random.nextInt(101),
-                height: 50.0 + random.nextInt(101),
-              ),
+        appBar: AppBar(
+          title: const Text("Stack Widget"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            ))
+                      ],
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            )),
+                      ],
+                    )),
+              ],
             ),
-          )),
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Text ini ada dilapisan tengah dari Stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Text ini ada dilapisan tengah dari Stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Text ini ada dilapisan tengah dari Stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Text ini ada dilapisan tengah dari Stack",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            RaisedButton(
+              child: Text("My Button"),
+              color: Colors.amber,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }

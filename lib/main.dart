f929ad35475f4bf91fd0e0c20d68b7ae47b062ca
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-22 17:40:53 +0700

    TextField Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/login_page.dart';
-import 'package:flutter_basic/main_page.dart';
-import 'package:flutter_basic/second_page.dart';
-import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: MainPage());
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({Key key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Media Query"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("TextField Widget")),
+        body: Container(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextField(
+                maxLength: 6,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text)
+            ],
+          ),
         ),
-        body: (MediaQuery.of(context).orientation == Orientation.portrait)
-            ? Column(
-                children: generateContainers,
-              )
-            : Row(
-                children: generateContainers,
-              ));
-  }
-
-  List<Widget> get generateContainers {
-    return [
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
       ),
-      Container(
-        color: Colors.green,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100,
-      ),
-    ];
+    );
   }
 }

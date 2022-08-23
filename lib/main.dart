9b17dbdc339efb54f6242a5e9f9adeeadd975714
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-23 10:36:30 +0700

    Prepare for Hero Animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(const MyApp());
-  });
+void main(List<String> args) {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
@@ -24,190 +19,63 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatelessWidget {
   const MainPage({Key key}) : super(key: key);
 
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Hero Animation",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const SecondPage();
+          }));
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: Container(
+              width: 100,
+              height: 100,
+              child: const Image(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://inikpop.com/wp-content/uploads/2022/05/Rose-thumbnail.png"))),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFEEEEEE),
-      body: Stack(
-        children: [
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
-            child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-            ),
-          ),
-          Positioned(
-            left: -getBigDiameter(context) / 4,
-            top: -getBigDiameter(context) / 4,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-              child: const Center(
-                child: Text(
-                  "dribblee",
-                  style: TextStyle(
-                      fontFamily: "Pacifico",
-                      fontSize: 30,
-                      color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            right: -getBigDiameter(context) / 2,
-            bottom: -getBigDiameter(context) / 2,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Color(0xFFF3E9EE)),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ListView(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  child: Column(
-                    children: const [
-                      TextField(
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.email,
-                              color: Color(0xffff4891),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xffff4891))),
-                            labelText: "Email: ",
-                            labelStyle: TextStyle(color: Color(0xffff4891))),
-                      ),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                            icon: Icon(
-                              Icons.vpn_key,
-                              color: Color(0xffff4891),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xffff4891))),
-                            labelText: "Password: ",
-                            labelStyle: TextStyle(color: Color(0xffff4891))),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                    child: const Text(
-                      "FORGOT PASSWORD?",
-                      style: TextStyle(color: Color(0xffff4891), fontSize: 11),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: const LinearGradient(
-                                colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter),
-                          ),
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.transparent,
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              splashColor: Colors.amber,
-                              onTap: () {},
-                              child: const Center(
-                                child: Text(
-                                  "SIGN IN",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      FloatingActionButton(
-                        elevation: 0,
-                        mini: true,
-                        onPressed: () {},
-                        child: const Image(
-                            image: AssetImage("assets/facebook.png")),
-                      ),
-                      FloatingActionButton(
-                        elevation: 0,
-                        mini: true,
-                        onPressed: () {},
-                        child: const Image(
-                            image: AssetImage("assets/twitter.png")),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      "DON'T HAVE AN ACCOUNT? ",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                          fontSize: 11,
-                          color: Color(0XFFFF4891),
-                          fontWeight: FontWeight.w700),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+      backgroundColor: Colors.pink[50],
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Hero Animation",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Container(
+              width: 200,
+              height: 200,
+              child: const Image(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://inikpop.com/wp-content/uploads/2022/05/Rose-thumbnail.png"))),
+        ),
       ),
     );
   }

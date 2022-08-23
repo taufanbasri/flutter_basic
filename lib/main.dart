208e316128fd717556402e5272cd8df5afcad584
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-08-23 13:10:44 +0700

    Google Fonts Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -9,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
+    return MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.srirachaTextTheme()),
+      home: const MainPage(),
     );
   }
 }
@@ -20,16 +22,26 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const fontcolor = Color(0xffffb401);
+
     return Scaffold(
       backgroundColor: const Color(0xff1e252d),
       appBar: AppBar(
         backgroundColor: const Color(0xff282f37),
         title: const Text("Google Fonts"),
       ),
-      body: const Center(
-        child: Text(
-          "I'll keep learning Flutter!",
-          style: TextStyle(color: Color(0xffffb401)),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "I'll keep learning Flutter!",
+              style: GoogleFonts.lato(color: fontcolor, fontSize: 25),
+            ),
+            const Text(
+              "Hello World!",
+              style: TextStyle(color: fontcolor),
+            )
+          ],
         ),
       ),
     );
